908ec073f2d4ccca7434ef8d25acb53da85c9c38
deepcrazy <312.gupta@gmail.com>
2019-10-21 19:03:10 -0400

    Implemented task 2_outputting, modified js and css file along with some amendments in index.html as well

--- 2_outputting/js/myScript.js
+++ 2_outputting/js/myScript.js
@@ -1,5 +1,14 @@
 try {
-    alert("Welcome to the Blockchain World..!!")
+
+    var title = document.getElementById('title');
+    title.innerHTML = "Task: 2_outputting";
+
+    var container = document.getElementById('welcomeMsgContainer');
+    var welcomeMsg = "Welcome to Task: \"2_outputting\" of FS2_JS_Client";
+
+    var pTag = document.getElementById('welcomeMsg');
+    pTag.innerHTML = welcomeMsg;
+
 } catch (error) {
     document.getElementById("welcomeMsg").innerHTML = error.message;
 } finally {
