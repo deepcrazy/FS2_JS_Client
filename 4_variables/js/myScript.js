3bf77ed77361f27a7979d3e4c2325eb72a13e881
deepcrazy <312.gupta@gmail.com>
2019-10-21 20:27:43 -0400

    Modified titles in index.html for 0,1,3 tasks and modified js, css and index.html for task 4 as well

--- 4_variables/js/myScript.js
+++ 4_variables/js/myScript.js
@@ -1,6 +1,6 @@
 try {
     var wordsArray = ['Universities', 'Colleges', 'Schools'];
-    var numArray = [7,5,9];
+    var numArray = [7, 5, 9];
     var listContainer = document.getElementById('listContainer');
     var lists = document.getElementById('list');
 
@@ -10,7 +10,7 @@ try {
         listTag.appendChild(listTagText);
         lists.appendChild(listTag);
     });
-    console.log("Successfully printed the lists of words along with the list of numbers..!!")
+    console.log("Successfully printed the lists of words along with their accompanying numbers from the list..!!")
 } catch (error) {
     console.log("Some error has occured: " + error);
 }
