756484df12981380bc6c12ed6b41e3d714ddbedb
deepcrazy <312.gupta@gmail.com>
2019-10-21 20:12:56 -0400

    Implemented Task 3_lists and modified js file along with index.html and css file too

--- 3_lists/js/myScript.js
+++ 3_lists/js/myScript.js
@@ -1,6 +1,23 @@
 try {
-    alert("Lists created successfully..!! Please check console message..!!")
-    console.log("Successfully generated lists..!!")
+    var citiesLists = document.getElementById('citiesList').innerText;
+    var fruitsLists = document.getElementById('fruitsLists').innerText;
+    var eduInstitutesLists = document.getElementById('eduInsitutesLists').innerText;
+
+    window.onloadend = userAlertMsg();
+
+    function userAlertMsg() {
+        setTimeout(() => {
+            alertMsg = "Following is the 1st list: List of cities: \n" + citiesLists;
+            alertMsg += "\n\nFollowing is the 2nd list: Type of Fruits: \n" + fruitsLists;
+            alertMsg += "\n\nFollowing is the 3rd list: Type of education institues: \n" + eduInstitutesLists;
+
+            alert(alertMsg);
+        }, 1000);
+    }
+
+    console.log("Lists displayed successfully on HTML page..!!");
+    console.log("Also, alert is providing information of words in the lists..!!");
+
 } catch (error) {
     console.log("Some error has occured: " + error.message);
 }
